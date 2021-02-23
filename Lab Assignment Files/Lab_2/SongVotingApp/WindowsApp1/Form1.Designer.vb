641a7ad3281f523fc47ee68fefcc633a863a32db
heyzuess <heyzuess@users.noreply.github.com>
2021-02-22 22:54:52 -0800

    WIP.ch.02_02.22.2021

---
+++ Lab Assignment Files/Lab_2/SongVotingApp/WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,171 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Song1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Song2 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Song3 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(127, 52)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(139, 21)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.Location = New System.Drawing.Point(24, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(287, 282)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(31, 55)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(79, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Select a genre:"
+        '
+        'Song1
+        '
+        Me.Song1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Song1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Song1.Location = New System.Drawing.Point(56, 108)
+        Me.Song1.Name = "Song1"
+        Me.Song1.Size = New System.Drawing.Size(234, 40)
+        Me.Song1.TabIndex = 3
+        Me.Song1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(7, 108)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(43, 40)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "1"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label3
+        '
+        Me.Label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(7, 167)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(43, 40)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "2"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Song2
+        '
+        Me.Song2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Song2.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Song2.Location = New System.Drawing.Point(56, 167)
+        Me.Song2.Name = "Song2"
+        Me.Song2.Size = New System.Drawing.Size(234, 40)
+        Me.Song2.TabIndex = 5
+        Me.Song2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label5
+        '
+        Me.Label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(7, 228)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(43, 40)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "3"
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Song3
+        '
+        Me.Song3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Song3.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Song3.Location = New System.Drawing.Point(56, 228)
+        Me.Song3.Name = "Song3"
+        Me.Song3.Size = New System.Drawing.Size(234, 40)
+        Me.Song3.TabIndex = 7
+        Me.Song3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Song3)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Song2)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Song1)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.ComboBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(315, 6)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(299, 288)
+        Me.GroupBox1.TabIndex = 9
+        Me.GroupBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(621, 322)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Song Voting App"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Song1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Song2 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Song3 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+End Class
---
+++ Lab Assignment Files/Lab_2/SongVotingApp/WindowsApp1/Form1.vb
@@ -0,0 +1,140 @@
+﻿'============================================================================
+' Author.: Jesse Iberri
+' Program: SongVotingApp
+' Date...: 02/22/2021
+' Purpose: Lab 2
+'============================================================================
+Public Class Form1
+    ' Private class to hold the SongGenre object
+    Private Class SongGenre
+        Public Name As String
+        Public Image As Object = Nothing
+        Public SongList As List(Of String) = Nothing
+
+        Public Sub New(GenreName As String)
+            Me.New(GenreName, Nothing)
+        End Sub
+
+        Public Sub New(GenreName As String, GenreImage As Object)
+            Name = GenreName
+            Image = GenreImage
+        End Sub
+    End Class
+
+    ' A dictionary to hold genre + image + songlist for quick selection
+    Dim myLib As New Dictionary(Of String, Object)
+
+    ' A List to hold the song item display objects
+    Dim songDisplay As New List(Of Object)
+
+    ' Declare constructor for form class
+    Public Sub New()
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        myLib.Add("", New SongGenre("", My.Resources.Music))
+        myLib.Add("Rock", New SongGenre("Rock", My.Resources.Electric))
+        myLib.Add("Country", New SongGenre("Country", My.Resources.Steel))
+        myLib.Add("Hip-Hop", New SongGenre("Hip-Hop", My.Resources.Classical))
+
+        ' Add some songs for each genre
+        ' Each genre list will be 3 long
+        Dim tempList As List(Of String)
+        Dim tempGenre As SongGenre = Nothing
+
+        ' Loop through each genre and add 3 songs to each list
+        For Each key In myLib.Keys
+            ' Add genres to combo box
+            ComboBox1.Items.Add(key)
+
+            ' Get current genre object from selected genre for non blank genres
+            If myLib.TryGetValue(key, tempGenre) And key > "" Then
+                ' Clear temp list
+                tempList = New List(Of String)
+
+                ' Add 3 generic songs to song list object
+                For i As Integer = 0 To 2
+                    Dim tempString As String = key.ToString() + " song " + (i + 1).ToString()
+
+                    ' Test
+                    'MsgBox("Key:  " + key + Chr(10) + "Index: " + i.ToString() + Chr(10) + "String: " + tempString)
+
+                    tempList.Add(tempString)
+                Next
+
+                ' Add song list object to dictionary for genre
+                tempGenre.SongList = tempList
+
+                ' Test
+                'MsgBox("genre: " + tempGenre.Name + Chr(10) + "valid list: " + IsNothing(tempList).ToString())
+            End If
+        Next
+
+        ' Keep song entry controls in a index to reference it later for updating
+        songDisplay.Add(Song1)
+        songDisplay.Add(Song2)
+        songDisplay.Add(Song3)
+
+        ' Test
+        'MsgBox(songDisplay.Count.ToString())
+
+        ' Set initial image and selection text
+        PictureBox1.Image = GetSelectionImage(ComboBox1.Text)
+        UpdateSongDisplay(ComboBox1.Text)
+    End Sub
+
+    'On select of option in combo box
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        ' Set the image from the currently selected genre
+        PictureBox1.Image = GetSelectionImage(ComboBox1.Text)
+        UpdateSongDisplay(ComboBox1.Text)
+    End Sub
+
+    ' A function to return the image object from the dictionary index
+    Function GetSelectionImage(SelectionText As String) As Object
+        ' Initialize returnVal object as null
+        Dim returnVal As Object = Nothing
+        Dim tempGenre As SongGenre = Nothing
+
+        ' Get image from dictionary index based on key
+        If myLib.TryGetValue(SelectionText, tempGenre) Then
+            returnVal = tempGenre.Image
+        End If
+
+        Return returnVal
+    End Function
+
+    Private Sub UpdateSongDisplay(GenreName As String)
+        Dim tempList As List(Of String) = Nothing
+        Dim tempGenre As SongGenre = Nothing
+
+        Dim hasValues As Boolean
+        hasValues = myLib.TryGetValue(GenreName, tempGenre)
+
+        ' Test
+        'MsgBox("Genre is " + GenreName)
+
+        ' If genre has a song list
+        If hasValues And GenreName <> "" Then
+            ' Test
+            'MsgBox("Number of songs: " + tempGenre.SongList.Count.ToString())
+
+            tempList = tempGenre.SongList
+            For i As Integer = 0 To tempList.Count - 1
+                ' Test
+                'MsgBox("Song number: " + i.ToString())
+
+                songDisplay(i).Text = tempList(i)
+            Next
+        Else
+            ' If no song list for genre, then clear selection
+            For i As Integer = 0 To songDisplay.Count - 1
+                ' Test
+                'MsgBox("Clearing song label " + i.ToString())
+
+                songDisplay(i).Text = ""
+            Next
+        End If
+    End Sub
+End Class
